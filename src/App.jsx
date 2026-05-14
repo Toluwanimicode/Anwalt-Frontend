8ba9c7eb73477201ea42945b83e69571bde5fdfd
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-05-14 18:24:59 +0100

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Carousel1 from './components/Carousel/Carousel'
+import Home from './components/Pages/Home/Home'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Carousel1 />
+      <Home />
       
     </>
   )
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
@@ -22,11 +23,11 @@ const Home = () => {
           <p>A security is an investment in a business . Make the 1st step</p>
         </div>
       </div>
-      <div>
+      <div className='body-2'>
         <div>
           <h3>We're there for you</h3>
           <p>With 25 years of experience in law and justice, our team is focused on delivering quality and value to our clients. We achieve this through practical and innovative legal solutions that help our clients . Everyone deserves support on the way to Justice </p>
-          <img />
+          <img src='./images/signature.png' />
         </div>
         <div>
           <img src='./images/h1-img-1.jpg'  />
