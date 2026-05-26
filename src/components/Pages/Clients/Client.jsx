01f3438f1bc9bc954099866001ccd0f6959f0296
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-05-26 17:01:54 +0100

    added changes

---
+++ src/components/Pages/Clients/Client.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Client = () => {
+    return (
+    <div>
+        <div>
+            <h2>Our Clients</h2>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <h3>List of our satisfied CLients</h3>
+            <p>impressive number of successful law cases in our portfolio is a confirmation of our quality . Check out the list of our clients and become one of them</p>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+        
+    </div>
+)
+}
+
+export default Client
