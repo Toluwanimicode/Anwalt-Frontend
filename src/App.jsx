4bd3bb1c94523007d865b0fe09781d80fdc50327
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-06-01 17:46:40 +0100

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Carousel1 from './components/Carousel/Carousel'
 import About from './components/Pages/AboutUs/About';
+import Client from './components/Pages/Clients/Client';
 import Home from './components/Pages/Home/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,6 +14,8 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About/>} />
+              <Route path="/about" element={<Client/>} />
+              <Route path="/about" element={<Team />} />
           </Routes>
         </BrowserRouter>
 
