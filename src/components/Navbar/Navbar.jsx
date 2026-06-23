3fda2d4a0e5b85dcafaf28a0de3373cc3e5e0a8a
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-06-23 14:29:23 +0100

    added changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,8 +23,8 @@ function NavigationBar() {
             <Nav.Link href="/about">About Us</Nav.Link>
             <Nav.Link href="/team">Our Team</Nav.Link>
             <Nav.Link href="/client">Clients</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
             <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
