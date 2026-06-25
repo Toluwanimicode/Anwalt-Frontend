11d04ff02d8ff2502e319b453a0af92f54207782
Toluwanimi Adenekan <tolugodsproperty@gmail.com>
2026-06-25 12:43:11 +0100

    added changes

--- src/components/Pages/Services/Services.jsx
+++ src/components/Pages/Services/Services.jsx
@@ -3,8 +3,36 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-        Services</div>
-  )
+        
+        <div>
+            <h3>Law Services</h3>
+        </div>
+        <div>
+            <div>
+                <a></a>
+                <h4>
+                    Best Law pratices
+                </h4>
+                <p>Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.</p>
+            </div>
+            <div>
+                <img />
+                <h4>
+                    Knowledge & practice
+                </h4>
+                <p>
+                    Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
+                </p>
+            </div>
+        </div>
+        <div>
+            <p>Special offer this month</p>
+            <h4>20% discount</h4>
+            <button>More</button>
+        </div>
+        
+    </div>
+)
 }
 
 export default Services
